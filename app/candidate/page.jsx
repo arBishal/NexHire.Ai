b4b3e140a0d6d3198cd9aa34c4f54643b4e869f9
arBishal <arbishal.319@gmail.com>
2025-06-04 05:25:31 +0600

    feat: add under construction routes

---
+++ app/candidate/page.jsx
@@ -0,0 +1,19 @@
+import Button from "@/components/button";
+
+export default function Intro() {
+  return (
+    <div className="flex flex-col gap-12 items-center justify-center w-full h-full">
+      <section
+        id="candidate"
+        className="max-w-5xl w-full h-full text-center flex flex-col items-center justify-center gap-4 px-6 pt-32"
+      >
+        <h1 className="text-4xl sm:6xl font-bold">
+          This Page is Under Construction
+        </h1>
+        <Button href="/" variant="ghost">
+          Go back to Home
+        </Button>
+      </section>
+    </div>
+  );
+}
