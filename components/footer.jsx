87db0a05605701b5c8f7463637565ac0b6489218
arBishal <arbishal.319@gmail.com>
2025-06-22 00:59:59 +0600

    chore: change footer

--- components/footer.jsx
+++ components/footer.jsx
@@ -2,8 +2,9 @@ export default function Footer() {
   return (
     <div className="px-6 py-4 w-full border-t border-border-transparent font-mono text-sm sm:text-base">
       <div className="max-w-5xl m-auto flex gap-2 flex-col sm:flex-row justify-between items-center">
-        <span className="text-left">NexHire.Ai &copy; 2025 </span>
-        <span className="text-right">Being created by: <a href="https://github.com/arBishal" className="hover:underline">arBishal</a></span>
+        <span className="text-left"> NexHire.Ai &copy; 2025 </span>
+        <span className="text-right"> Initiative by: CyberdineSystems </span>
+        <a href="https://github.com/arBishal" className="hover:underline"> Github Repository </a>
       </div>
     </div>
   );
